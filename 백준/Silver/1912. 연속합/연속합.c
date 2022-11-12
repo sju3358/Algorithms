132663ef4bb29d5c38aaec27082bb0d31dcc9751
조성락 <tjdfkr011@gmail.com>
2022-11-12 12:30:54 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.c
@@ -0,0 +1,1 @@
+int main(){int n,i,temp,sum = 0,max = -1001;scanf("%d", &n);for (i = 0; i < n; i++){scanf("%d", &temp);sum = sum + temp;if (max < sum)max = sum;if (sum < 0)sum = 0;}printf("%d", max);}
