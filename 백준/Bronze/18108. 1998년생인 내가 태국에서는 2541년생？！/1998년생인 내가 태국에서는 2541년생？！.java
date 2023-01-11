d257cc6cac3342d64e5563dbe2ccd627a970ec29
조성락 <tjdfkr011@gmail.com>
2023-01-11 14:19:41 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        
+    	Scanner sc = new Scanner(System.in);
+    	int result;
+    	int input = sc.nextInt();
+        
+        System.out.println(input-543);
+        sc.close();
+    }
+}
