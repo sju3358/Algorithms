15a1179b420a205e9f2c7fbf5c8d5488c70f3416
sju3358 <tjdfkr011.dev@gmail.com>
2023-02-14 17:48:35 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 2452 ms, Memory: 310880 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -7,43 +7,28 @@ public class Main {
 	public static void rotateArr() {
 		int[][] newArr = new int[N][M];
 		
-		int start_i = 0;
-		int start_j = 0;
-		
-		
-		for(int row = N, col = M; row > 0 && col > 0;) {
+		for(int row = N, col = M, start_i = 0, start_j = 0; row > 0 && col > 0;start_i++, start_j++, row -=2, col -= 2) {
 			
 			// ㅓ
-			for(int cnt = 0, j = start_j; cnt < col-1; cnt++) {
+			for(int cnt = 0, j = start_j; cnt < col-1; cnt++, j++)
 				newArr[start_i][j] = arr[start_i][j+1];
-				j++;
-			}
 				
 
 			// ㅜ			
-			for(int cnt = 0, i = start_i+1; cnt < row-1; cnt++) {
+			for(int cnt = 0, i = start_i+1; cnt < row-1; cnt++, i++)
 				newArr[i][start_j] = arr[i-1][start_j];
-				i++;
-			}
 				
 			
 			// ㅏ
-			for(int cnt = 0, j = start_j+1; cnt < col-1; cnt++) {	
+			for(int cnt = 0, j = start_j+1; cnt < col-1; cnt++, j++)
 				newArr[start_i+(row-1)][j] = arr[start_i+(row-1)][j-1];
-				j++;
-			}
+			
 			
 			
 			// ㅗ
-			for(int cnt = 0, i = start_i; cnt < row-1; cnt++) {
+			for(int cnt = 0, i = start_i; cnt < row-1; cnt++, i++)
 				newArr[i][start_j+(col-1)] = arr[i+1][start_j+(col-1)];
-				i++;
-			}
-						
-			start_i++;
-			start_j++;
-			row = row - 2;
-			col = col - 2;
+	
 		}
 		
 		
