888b078505ba17bad45c3d415f9b47719a7f2f5b
sju3358 <tjdfkr011.dev@gmail.com>
2023-02-14 13:55:47 +0900

    [level 2] Title: 프린터, Time: 0.06 ms, Memory: 3.65 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.cpp
+++ 프로그래머스/lv2/42587. 프린터/프린터.cpp
@@ -1,67 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
+	#include <iostream>
+	#include <vector>
+	#include <queue>
+	using namespace std;
 
 
-typedef struct document {
-	int index;
-	int prioirty;
-}Document;
+	typedef struct document {
+		int index;
+		int prioirty;
+	}Document;
 
-bool isMaxPrioirty(queue<Document> documents, int cnt) {
+	bool isMaxPrioirty(queue<Document> documents, int cnt) {
 
-	vector<Document> copiedDocuments;
+		vector<Document> copiedDocuments;
 
-	while (documents.empty() != true) {
-		copiedDocuments.push_back(documents.front());
-		documents.pop();
-	}
-
-	int prioirty = copiedDocuments[0].prioirty;
-
-	for (int i = 1; i < copiedDocuments.size(); i++)
-		if (copiedDocuments[i].prioirty > prioirty)
-			return false;
-
-	return true;
-}
-
-
-int solution(vector<int> priorities, int location) {
-	
-	queue<Document> documents;
-
-	int cnt = 1;
-
-	for (int i = 0; i < priorities.size(); i++) 
-		documents.push({ i, priorities[i] });
-	
-
-	while (1) {
-
-		if(isMaxPrioirty(documents, cnt)) {
-			if (documents.front().index == location)
-				break;
-			else
-			{
-				documents.pop();
-				cnt++;
-			}
-		}
-		else 
-		{
-			documents.push(documents.front());
+		while (documents.empty() != true) {
+			copiedDocuments.push_back(documents.front());
 			documents.pop();
 		}
+
+		int prioirty = copiedDocuments[0].prioirty;
+
+		for (int i = 1; i < copiedDocuments.size(); i++)
+			if (copiedDocuments[i].prioirty > prioirty)
+				return false;
+
+		return true;
 	}
 
-	return cnt;
-}
+
+	int solution(vector<int> priorities, int location) {
+	
+		queue<Document> documents;
+
+		int cnt = 1;
+
+		for (int i = 0; i < priorities.size(); i++) 
+			documents.push({ i, priorities[i] });
+	
+	
+		bool isMaxPrioirtyFlag = isMaxPrioirty(documents, cnt);
+		while(isMaxPrioirtyFlag != true || documents.front().index != location){
+
+			if(isMaxPrioirtyFlag == true)
+					cnt++;
+			else 
+				documents.push(documents.front());
+
+			documents.pop();
+			isMaxPrioirtyFlag = isMaxPrioirty(documents, cnt);
+		}
+
+		return cnt;
+	}
 
 
 
-// int main() {
-// 	cout << solution({2,1,3,2}, 2) << endl;
-// 	cout << solution({1,1,9,1,1,1}, 0) << endl;
-// }
+	 // int main() {
+	 // cout << solution({2,1,3,2}, 2) << endl;
+	 // cout << solution({1,1,9,1,1,1}, 0) << endl;
+	 // }
