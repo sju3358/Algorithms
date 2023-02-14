d1f146b34d44d4ec1c06eeba2009309236f1201d
sju3358 <tjdfkr011.dev@gmail.com>
2023-02-14 13:31:13 +0900

    [level 2] Title: 프린터, Time: 0.04 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+
+typedef struct document {
+	int index;
+	int prioirty;
+}Document;
+
+bool isMaxPrioirty(queue<Document> documents, int cnt) {
+
+	vector<Document> copiedDocuments;
+
+	while (documents.empty() != true) {
+		copiedDocuments.push_back(documents.front());
+		documents.pop();
+	}
+
+	int prioirty = copiedDocuments[0].prioirty;
+
+	for (int i = 1; i < copiedDocuments.size(); i++)
+		if (copiedDocuments[i].prioirty > prioirty)
+			return false;
+
+	return true;
+}
+
+
+int solution(vector<int> priorities, int location) {
+	queue<Document> documents;
+
+	int cnt = 1;
+
+	for (int i = 0; i < priorities.size(); i++) {
+		Document document;
+		document.index = i;
+		document.prioirty = priorities[i];
+		documents.push(document);
+	}
+
+	while (1) {
+
+		if (isMaxPrioirty(documents, cnt)) {
+			if (documents.front().index == location) {
+				return cnt;
+			}
+			else {
+				documents.pop();
+				cnt++;
+			}
+		}
+		else {
+			documents.push(documents.front());
+			documents.pop();
+		}
+
+	}
+}
+
+
+
+int main() {
+	cout << solution({2,1,3,2}, 2) << endl;
+	cout << solution({1,1,9,1,1,1}, 0) << endl;
+}
