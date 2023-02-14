2cdeb3cf379d3c96728f904680b8b2f971da55e7
sju3358 <tjdfkr011.dev@gmail.com>
2023-02-14 15:59:44 +0900

    [Bronze II] Title: 블랙잭, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -2,34 +2,26 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static int[] arr = new int[100];
-	
 	public static void main (String args[]) {
 		
 		Scanner scanner = new Scanner(System.in);
-		int n,m;
-	    int i,j,k;
-	    int dif = 999999999,result = 0;
-	    int temp;
+	
+	    int max = -1;
 
-	    n = scanner.nextInt();
-	    m = scanner.nextInt();
-
-	    for(i = 0; i<n; i++)
+	    int n = scanner.nextInt();
+	    int m = scanner.nextInt();
+	    int[] arr = new int[n];
+	    
+	    for(int i = 0; i<n; i++)
 	        arr[i] = scanner.nextInt();
 
-	    for(i = 0; i<n-2; i++)
-	        for(j=i+1; j<n-1; j++)
-	            for(k = j+1; k<n; k++)
-	            {
-	                temp = arr[i] + arr[j] + arr[k];
-	                int cnt = m-temp;
-	                if(cnt >= 0 && cnt < dif)
-	                {
-	                    dif = cnt;
-	                    result = temp;
-	                }
+	    for(int i = 0; i<n-2; i++)
+	        for(int j=i+1; j<n-1; j++)
+	            for(int k = j+1; k<n; k++){
+	                int score = arr[i] + arr[j] + arr[k];
+	                if(max < score && score <= m)
+	                	max = score;
 	            }
-	    System.out.println(result);
+	    System.out.println(max);
 	}
 }
