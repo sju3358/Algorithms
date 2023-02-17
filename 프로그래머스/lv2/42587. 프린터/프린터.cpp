3eb1a3b59765ada8f12b98c4fe2c59e0f618e8eb
sju3358 <tjdfkr011.dev@gmail.com>
2023-02-17 20:58:09 +0900

    [level 2] Title: 프린터, Time: 0.06 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.cpp
+++ 프로그래머스/lv2/42587. 프린터/프린터.cpp
@@ -1,61 +1,62 @@
-	#include <iostream>
-	#include <vector>
-	#include <queue>
-	using namespace std;
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
 
 
-	typedef struct document {
-		int index;
-		int prioirty;
-	}Document;
+typedef struct document {
+	int index;
+	int prioirty;
+}Document;
 
-	bool isMaxPrioirty(queue<Document> documents, int cnt) {
+bool isMaxPrioirty(queue<Document> documents) {
 
-		vector<Document> copiedDocuments;
+	vector<Document> copiedDocuments;
 
-		while (documents.empty() != true) {
-			copiedDocuments.push_back(documents.front());
-			documents.pop();
-		}
-
-		int prioirty = copiedDocuments[0].prioirty;
-
-		for (int i = 1; i < copiedDocuments.size(); i++)
-			if (copiedDocuments[i].prioirty > prioirty)
-				return false;
-
-		return true;
+	while (documents.empty() != true) {
+		copiedDocuments.push_back(documents.front());
+		documents.pop();
 	}
 
+	int prioirty = copiedDocuments[0].prioirty;
 
-	int solution(vector<int> priorities, int location) {
+	for (int i = 1; i < copiedDocuments.size(); i++)
+		if (copiedDocuments[i].prioirty > prioirty)
+			return false;
+
+	return true;
+}
+
+
+int solution(vector<int> priorities, int location) {
 	
-		queue<Document> documents;
+	queue<Document> documents;
 
-		int cnt = 1;
+	int cnt = 1;
 
-		for (int i = 0; i < priorities.size(); i++) 
-			documents.push({ i, priorities[i] });
+	//vector로 들어온 input을 queue에 담기.
+	for (int i = 0; i < priorities.size(); i++) 
+		documents.push({ i, priorities[i] });
 	
 	
-		bool isMaxPrioirtyFlag = isMaxPrioirty(documents, cnt);
-		while(isMaxPrioirtyFlag != true || documents.front().index != location){
+	bool isMaxPrioirtyFlag; //현재 front값이 max값인지 확인 , front가 target location인지 확인
+	while((isMaxPrioirtyFlag = isMaxPrioirty(documents)) != true || documents.front().index != location){
 
-			if(isMaxPrioirtyFlag == true)
-					cnt++;
-			else 
-				documents.push(documents.front());
+		//target location이 아니지만 max값이라면 pop하고 출력된 프린터 개수 세기
+		if(isMaxPrioirtyFlag == true)
+				cnt++;
+		else 
+			documents.push(documents.front()); //target location도 아니고 max값도 아니면 빼서 뒤로 넣기
 
-			documents.pop();
-			isMaxPrioirtyFlag = isMaxPrioirty(documents, cnt);
-		}
-
-		return cnt;
+		documents.pop();
 	}
 
+	return cnt;
+}
 
 
-	 // int main() {
-	 // cout << solution({2,1,3,2}, 2) << endl;
-	 // cout << solution({1,1,9,1,1,1}, 0) << endl;
-	 // }
+
+	// int main() {
+	// cout << solution({2,1,3,2}, 2) << endl;
+	// cout << solution({1,1,9,1,1,1}, 0) << endl;
+	// }
