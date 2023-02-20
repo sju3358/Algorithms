7b0d17b23d92e2a142ee1ceb1a194b3be2bd0e5b
sju3358 <tjdfkr011.dev@gmail.com>
2023-02-20 14:49:58 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.cpp
@@ -0,0 +1,74 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <string>
+
+#define INTEGER_MAX_VALUE 2100000000
+
+using namespace std;
+
+
+
+
+
+typedef struct Word {
+	string word;
+	int count;
+};
+
+int compareString(string a, string b) {
+	int diff = 0;
+
+	for (int i = 0; i < a.length(); i++) {
+		if (a[i] != b[i])
+			diff++;
+	}
+	return diff;
+}
+
+int solution(string begin, string target, vector<string> words) {
+	
+	int minCount = INTEGER_MAX_VALUE;
+
+	queue<Word> nextWord;
+
+	vector<bool> isVisit;
+	for (int i = 0; i < words.size(); i++)
+		isVisit.push_back(false);
+
+	for (int i = 0; i < words.size(); i++) {
+		if (compareString(begin, words[i]) <= 1) {
+			nextWord.push({ words[i],1 });
+			isVisit[i] = true;
+		}
+	}
+
+	bool isFind = false;
+	
+	while (nextWord.empty() != true) {
+		Word curWord = nextWord.front(); nextWord.pop();
+
+
+		if (curWord.word.compare(target) == 0) {
+			if (minCount > curWord.count)
+				minCount = curWord.count;
+			isFind = true;
+		}
+		else {
+			for (int i = 0; i < words.size(); i++) {
+				if (compareString(curWord.word,words[i]) <= 1 && isVisit[i] == false) {
+					isVisit[i] = true;
+					nextWord.push({ words[i], curWord.count + 1 });
+				}
+			}
+		}
+	}
+
+		
+	return isFind == true ? minCount : 0;
+}
+
+// int main() {
+
+// 	solution("hit", "cog", { "hot","dot","dog","lot","log","cog" });
+// }
