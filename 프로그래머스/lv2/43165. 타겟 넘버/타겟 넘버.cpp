8e4172dce21f78969aa28b88ad6da2a319d57ef6
sju3358 <tjdfkr011.dev@gmail.com>
2023-02-20 13:56:59 +0900

    [level 2] Title: 타겟 넘버, Time: 0.56 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,26 +3,29 @@
 #include <iostream>
 using namespace std;
 
+int TARGET = 0;
+int ANSWER = 0;
 
-void findAnswer(int curIndex, int curSum, vector<int> numbers, int target, int& answer) {
+void findAnswer(int curIndex, int curSum, vector<int> numbers) {
 	
 	if (curIndex == numbers.size()) {
-		if (curSum == target)
-			answer++;
+		if (curSum == TARGET)
+			ANSWER++;
 	}
 	else {
-		findAnswer(curIndex + 1, curSum + numbers[curIndex], numbers, target, answer);
-		findAnswer(curIndex + 1, curSum + (-1)*numbers[curIndex], numbers, target, answer);
+		findAnswer(curIndex + 1, curSum + numbers[curIndex], numbers);
+		findAnswer(curIndex + 1, curSum + (-1)*numbers[curIndex], numbers);
 	}
 }
 
 
 int solution(vector<int> numbers, int target) {
-	int answer = 0;
 
-	findAnswer(0, 0, numbers, target, answer);
+    TARGET = target;
+    
+	findAnswer(0, 0, numbers);
 
-	return answer;
+	return ANSWER;
 }
 
 // int main() {
