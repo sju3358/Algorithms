3821bff6e48b5ed98d7a65fa50389316daafb09c
sju3358 <tjdfkr011.dev@gmail.com>
2023-02-24 09:38:14 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> numbers;
+
+
+void getPermutation(int n, int m, vector<int> curIndex,int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < curIndex.size(); i++) {
+			cout << numbers[curIndex[i]] << " ";
+		}
+		cout << '\n';
+	}
+	else {
+		
+		int start = cnt == 0 ? 0 : curIndex[cnt - 1];
+		for (int i = start; i < n; i++) {
+				curIndex[cnt] = i;
+				getPermutation(n, m, curIndex, cnt + 1);
+		}
+	}
+}
+
+void nPm(int n, int m) {
+	vector<int> curIndex;
+	for (int i = 0; i < m; i++) 
+		curIndex.push_back(-1);
+	getPermutation(n, m, curIndex, 0);
+
+}
+
+
+int main() {
+
+	int n; cin >> n;
+	int m; cin >> m;
+	for (int i = 0; i < n; i++) {
+		int number; cin >> number;
+		numbers.push_back(number);
+	}
+
+	sort(numbers.begin(), numbers.end());
+
+	nPm(n,m);
+}
