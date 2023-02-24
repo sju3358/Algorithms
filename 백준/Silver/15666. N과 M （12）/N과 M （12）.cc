ccd2b2d0181a7ea0d2c82070f21c1e555fc354ce
sju3358 <tjdfkr011.dev@gmail.com>
2023-02-24 09:51:49 +0900

    [Silver II] Title: N과 M (12), Time: 4 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+vector<int> numbers;
+unordered_set<string> permutations;
+
+void getPermutation(int n, int m, vector<int> curIndex, int cnt) {
+	if (cnt == m) {
+		string seq;
+
+		for (int i = 0; i < curIndex.size(); i++) {
+			seq += '0' + numbers[curIndex[i]];
+		}
+
+		auto iter = permutations.find(seq);
+		if (iter == permutations.end()){
+			for (int i = 0; i < curIndex.size(); i++) {
+				cout << numbers[curIndex[i]] << " ";
+			}
+			cout << '\n';
+			permutations.insert(seq);
+		}
+
+
+
+		
+	}
+	else {
+		int start = cnt == 0 ? 0 :  curIndex[cnt - 1];
+		for (int i = start; i < n; i++) {
+
+			curIndex[cnt] = i;
+			getPermutation(n, m, curIndex, cnt + 1);
+			
+		}
+	}
+}
+
+void nPm(int n, int m) {
+	vector<int> curIndex;
+
+	for (int i = 0; i < m; i++) 
+		curIndex.push_back(-1);
+
+	getPermutation(n, m, curIndex, 0);
+
+}
+
+
+int main() {
+
+	int n; cin >> n;
+	int m; cin >> m;
+	for (int i = 0; i < n; i++) {
+		int number; cin >> number;
+		numbers.push_back(number);
+	}
+
+	sort(numbers.begin(), numbers.end());
+
+	nPm(n,m);
+}
