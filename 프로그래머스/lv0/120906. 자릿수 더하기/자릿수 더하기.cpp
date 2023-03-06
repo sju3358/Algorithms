d536c71ba41a6d790725efb63832cfab6467ede6
sju3358 <tjdfkr011.dev@gmail.com>
2023-03-06 10:35:56 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    while(n){
+        answer += n%10;
+        n = n / 10;
+    }
+    return answer;
+}
