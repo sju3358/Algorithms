fb504ff21d1973f9a55cd8a41cab1bd23a787a77
sju3358 <tjdfkr011.dev@gmail.com>
2023-03-14 19:43:36 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,69 +1,55 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <sstream>
+#include <set>
+#include <iostream>
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
-    vector<string>::iterator iteratorOfOperations;
+    vector<int> answer;
+    multiset<int> dualPrioirtyQueue;
 
-    vector<int> temp;
-    vector<int>::iterator iteratorOfTemp;
+    for(int i = 0; i < operations.size(); i++){
+        string input = operations[i];
 
-    priority_queue<int> two_way_priority_queue;
-    
-    for(iteratorOfOperations = operations.begin(); iteratorOfOperations != operations.end(); iteratorOfOperations++){
-        
-        stringstream input(*iteratorOfOperations);
-        string output[2];
-        input >> output[0] >> output[1];
-        
-        string operation = output[0];
-        int value = stoi(output[1]);
+        if(input[0] =='I'){
+            input = input.substr(2);
+            dualPrioirtyQueue.insert(stoi(input));
+        }
+        else{
+            input = input.substr(2);
 
-        if(!operation.compare("I"))
-            two_way_priority_queue.push(value);
-
-        else if(!operation.compare("D")){
-
-            if(value == 1){
-                
-                if(two_way_priority_queue.size() > 0)
-                    two_way_priority_queue.pop();
-            }
-            else if(value == -1){
-
-                if(two_way_priority_queue.size() > 0)
-                {
-                    while(two_way_priority_queue.size() > 1){
-                        temp.push_back(two_way_priority_queue.top());
-                        two_way_priority_queue.pop();
-                    }
-                    two_way_priority_queue.pop();
+            if(stoi(input) == 1){
+                if(dualPrioirtyQueue.empty() != true){
+                    auto iter = dualPrioirtyQueue.end();
+                    iter--;
+                    dualPrioirtyQueue.erase(iter);
                 }
-
-                for(iteratorOfTemp = temp.begin(); iteratorOfTemp != temp.end(); iteratorOfTemp++)
-                    two_way_priority_queue.push(*iteratorOfTemp);
-
-                while(!temp.empty())
-                    temp.pop_back();
-
+            }
+            else{
+                if(dualPrioirtyQueue.empty() != true){
+                    auto iter = dualPrioirtyQueue.begin();
+                    dualPrioirtyQueue.erase(iter);
+                }
             }
         }
     }
-    
-    vector<int> answer;
 
-    if(two_way_priority_queue.size() == 0){
+
+    if(dualPrioirtyQueue.empty() == true){
         answer.push_back(0);
         answer.push_back(0);
     }
     else{
-        answer.push_back(two_way_priority_queue.top());
-        while(two_way_priority_queue.size() > 1)
-            two_way_priority_queue.pop();
-        answer.push_back(two_way_priority_queue.top());
+        auto iter = dualPrioirtyQueue.end(); iter--;
+        answer.push_back(*iter);
+        iter = dualPrioirtyQueue.begin();
+        answer.push_back(*iter);
     }
-    
+
     return answer;
+}
+
+int main(){
+    for(int num : solution({"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"}))
+        cout << num;
 }
