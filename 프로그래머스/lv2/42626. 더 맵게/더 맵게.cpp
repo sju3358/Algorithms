9666dcb549004be6d19868afd7b6e5da6cfb0200
sju3358 <tjdfkr011.dev@gmail.com>
2023-03-14 19:32:33 +0900

    [level 2] Title: 더 맵게, Time: 249.40 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
@@ -3,14 +3,6 @@
 #include <queue>
 using namespace std;
 
-int getMixedFood(int food1, int food2) {
-	if (food1 < food2)
-		return food1 + (food2 * 2);
-	else
-		return (food1 * 2) + food2;
-}
-
-
 int solution(vector<int> scoville, int K) {
 	int answer = 0;
 	
@@ -21,19 +13,18 @@ int solution(vector<int> scoville, int K) {
 
 	while (foods.empty() != true) {
 
-		int food1 = foods.top();  foods.pop();
-
-		if (food1 >= K)
+		if (foods.top() >= K)
 			return answer;
-		else {
+		else{
+			int food1 = foods.top();  foods.pop();
+			
 			if (foods.empty() == true)
 				return -1;
 			else {
 				int food2 = foods.top(); foods.pop();
-				foods.push(getMixedFood(food1, food2));
+				foods.push(food1 + (food2 * 2));
 				answer++;
 			}
 		}
-		
 	}
 }
