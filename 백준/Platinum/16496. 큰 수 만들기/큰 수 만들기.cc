d01ac3f505b930731ef78d1183beb626d7a0a4fd
sju3358 <tjdfkr011.dev@gmail.com>
2023-04-13 12:15:40 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 4 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.cc
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+bool compare(string a, string b) {
+
+    string targetA = a + b;
+    string targetB = b + a;
+
+    for(int i = 0 ; i < targetA.length(); i++){
+        if(targetA[i] != targetB[i])
+            return targetA[i] > targetB[i];
+    }
+    return false;
+}
+
+
+string solution(vector<int>& numbers){
+
+    string answer = "";
+
+    vector<string> stringsOfNumbers;
+    for (int i = 0; i < numbers.size(); i++) {
+        stringsOfNumbers.push_back(to_string(numbers[i]));
+    }
+
+    sort(stringsOfNumbers.begin(), stringsOfNumbers.end(), compare);
+
+    for (int i = 0; i < stringsOfNumbers.size(); i++)
+        answer += stringsOfNumbers[i];
+
+    if (answer[0] == '0')
+        answer = "0";
+
+    return answer;
+
+}
+
+int main(void) {
+    int n; cin >> n;
+    vector<int> numbers(n);
+    for(int i = 0; i < n; i++){
+        int number; cin >> number;
+        numbers[i] = number;
+    }
+    cout << solution(numbers);
+}
