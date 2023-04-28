485377d1492324515406f5afd3a1a93e3022f18c
sju3358 <tjdfkr011.dev@gmail.com>
2023-04-28 10:46:56 +0900

    [Gold IV] Title: 돌 그룹, Time: 384 ms, Memory: 184132 KB -BaekjoonHub

---
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+
+int solution(vector<int> stones){
+
+    vector<vector<vector<bool>>> visited(1001, vector<vector<bool>>(1001, vector<bool>(1001,false)));
+
+    sort(stones.begin(), stones.end());
+    queue<vector<int>> nextStones; nextStones.push(stones);
+    visited[stones[0]][stones[1]][stones[2]] = true;
+
+    bool canMake = false;
+
+    while(nextStones.empty() != true){
+
+        vector<int> curStones = nextStones.front(); nextStones.pop();
+
+        if(curStones[0] == curStones[1] && curStones[1] == curStones[2]){
+            canMake = true;
+            break;
+        }
+
+        sort(curStones.begin(),curStones.end());
+
+
+        if(curStones[0] != curStones[1] &&  curStones[0]*2 <= 1000){
+            int i = curStones[0]*2;
+            int j = curStones[1] - curStones[0];
+            int k = curStones[2];
+            if(visited[i][j][k] != true) {
+                nextStones.push({i, j, k});
+                visited[i][j][k] = true;
+            }
+        }
+
+        if(curStones[1] != curStones[2] && curStones[1]*2 <= 1000){
+            int i = curStones[0];
+            int j = curStones[1]*2;
+            int k = curStones[2]- curStones[1];
+            if(visited[i][j][k] != true) {
+                nextStones.push({i, j, k});
+                visited[i][j][k] = true;
+            }
+        }
+
+        if(curStones[0] != curStones[2] && curStones[0]*2 <= 1000){
+            int i = curStones[0]*2;
+            int j = curStones[1] ;
+            int k = curStones[2]- curStones[0];
+            if(visited[i][j][k] != true) {
+                nextStones.push({i, j, k});
+                visited[i][j][k] = true;
+            }
+        }
+
+
+
+    }
+
+    if(canMake == false)
+        return 0;
+    else
+        return 1;
+}
+
+int main(){
+
+    vector<int> stones(3);
+    for(int i = 0; i <3; i++)
+        cin >> stones[i];
+
+    cout << solution(stones);
+}
