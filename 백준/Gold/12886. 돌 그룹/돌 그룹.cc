194a4d3790ecf1a516fb95c117def19ac15562e2
sju3358 <tjdfkr011.dev@gmail.com>
2023-04-28 10:52:20 +0900

    [Gold IV] Title: 돌 그룹, Time: 32 ms, Memory: 4164 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.cc
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.cc
@@ -7,11 +7,11 @@ using namespace std;
 
 int solution(vector<int> stones){
 
-    vector<vector<vector<bool>>> visited(1001, vector<vector<bool>>(1001, vector<bool>(1001,false)));
+    vector<vector<bool>> visited(1001, vector<bool>(1001, false));
 
     sort(stones.begin(), stones.end());
     queue<vector<int>> nextStones; nextStones.push(stones);
-    visited[stones[0]][stones[1]][stones[2]] = true;
+    visited[stones[0]][stones[1]] = true;
 
     bool canMake = false;
 
@@ -31,9 +31,9 @@ int solution(vector<int> stones){
             int i = curStones[0]*2;
             int j = curStones[1] - curStones[0];
             int k = curStones[2];
-            if(visited[i][j][k] != true) {
+            if(visited[i][j] != true) {
                 nextStones.push({i, j, k});
-                visited[i][j][k] = true;
+                visited[i][j] = true;
             }
         }
 
@@ -41,9 +41,9 @@ int solution(vector<int> stones){
             int i = curStones[0];
             int j = curStones[1]*2;
             int k = curStones[2]- curStones[1];
-            if(visited[i][j][k] != true) {
+            if(visited[i][j] != true) {
                 nextStones.push({i, j, k});
-                visited[i][j][k] = true;
+                visited[i][j] = true;
             }
         }
 
@@ -51,9 +51,9 @@ int solution(vector<int> stones){
             int i = curStones[0]*2;
             int j = curStones[1] ;
             int k = curStones[2]- curStones[0];
-            if(visited[i][j][k] != true) {
+            if(visited[i][j] != true) {
                 nextStones.push({i, j, k});
-                visited[i][j][k] = true;
+                visited[i][j] = true;
             }
         }
 
