47ec8b6984d3d1dfdfe570ea9d99af1b4b02d8b9
sju3358 <tjdfkr011.dev@gmail.com>
2023-07-24 13:02:15 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 180 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,12 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
