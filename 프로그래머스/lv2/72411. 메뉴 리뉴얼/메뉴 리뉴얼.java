b3ac8184f549b78a6d81037cdc81ea21d7b3dde5
sju3358 <tjdfkr011.dev@gmail.com>
2023-07-24 20:27:28 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 5.10 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,18 +1,23 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution {
 
     private static boolean[][] orderInfos;
     private static ArrayList<String> menuList = new ArrayList<>();
+    private static List<Character> orderList = new ArrayList<>();
+
+
+    private boolean[] check = new boolean[26];
+    private char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
 
     private int maxCount = 0;
-    private List<String> tempMenuList;
+    private Set<String> tempMenuList;
+
 
     public boolean check(int[] combination, boolean[] orderInfo){
         for(int i = 0; i < combination.length; i++){
-            if(orderInfo[combination[i]] == false)
+            if(orderInfo[orderList.get(combination[i]) - 'A'] == false)
                 return false;
         }
         return true;
@@ -22,14 +27,18 @@ class Solution {
         if(curLength == combination.length){
             int count = 0;
 
-            for(int i = 0; i < combination.length; i++){
+            for(int i = 0; i < orderInfos.length; i++){
                 if(check(combination, orderInfos[i]) == true)
                     count++;
             }
 
+            if(count < 2)
+                return;
+
             String menu = "";
             for(int i = 0; i < combination.length; i++)
-                menu = menu + ('A' + combination[i]);
+                menu = menu + orderList.get(combination[i]);
+
 
             if(count > maxCount){
                 maxCount = count;
@@ -42,11 +51,11 @@ class Solution {
         } else {
             int curIndex;
             if(curLength != 0)
-                curIndex = combination[curLength-1];
+                curIndex = combination[curLength-1]+1;
             else
                 curIndex = 0;
 
-            for(int i = curIndex+1; i < 24; i++){
+            for(int i = curIndex; i < orderList.size(); i++){
                 combination[curLength] = i;
                 makeCombination(curLength+1, combination);
             }
@@ -55,18 +64,36 @@ class Solution {
 
     public String[] solution(String[] orders, int[] course) {
 
-        orderInfos = new boolean[orders.length][24];
+        orderInfos = new boolean[orders.length][26];
 
         for(int i = 0; i < orders.length; i++){
             String order = orders[i];
-            for(int j = 0; j < order.length(); j++)
-                orderInfos[i][order.charAt(j) - 'A'] = true;
+            for(int j = 0; j < order.length(); j++) {
+                int index = order.charAt(j) - 'A';
+                orderInfos[i][index] = true;
+                check[index] = true;
+            }
         }
 
+        for(int i = 0; i <check.length; i++)
+            if(check[i] == true)
+                orderList.add(alphabet[i]);
+
+
         for(int i = 0; i < course.length; i++){
+
+            int orderCount = 0;
+            for(int j = 0; j < orderInfos.length; j++)
+                if ( orderInfos[j].length >= course[i])
+                    orderCount++;
+
+            if(orderCount < 2)
+                continue;
+
+            maxCount = 0;
             int N = course[i];
             int[] combination = new int[N];
-            tempMenuList = new ArrayList<>();
+            tempMenuList = new HashSet<>();
             makeCombination(0,combination);
 
             for(String menu : tempMenuList)
@@ -74,13 +101,15 @@ class Solution {
         }
 
         String[] answer = new String[menuList.size()];
-        return menuList.toArray(answer);
+        menuList.toArray(answer);
+        Arrays.sort(answer);
+        return answer;
     }
 
     public static void main(String args[]){
         System.out.println(Arrays.toString(new Solution().solution(
-                new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"},
-                new int[]{2,3,4}
+                new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"},
+                new int[]{2,3,5}
         )));
     }
 }
